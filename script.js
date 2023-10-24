cf730f4299d1f82d222a68e07feb11ea3020964c
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-24 10:23:44 -0700

    aaaa

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //Grabs the "toDoArea" element and makes a new element for the "toDoItem"s
     const toDoArea = document.getElementById("toDoArea");
     const toDoItem = document.createElement("p");
-    toDoItem.innerText = "placehold";
+    toDoItem.innerText = "meep";
     toDoArea.appendChild(toDoItem);
   });
   addItem.addEventListener("click")
